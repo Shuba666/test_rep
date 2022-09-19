ac7cb971f073ab3ca17e3579e03af6b03a5eb177
Shuba666 <evgeniyeliakov@gmail.com>
2022-09-19 17:33:16 +0300

    'update my test 2'

--- hungry.py
+++ hungry.py
@@ -1,5 +1,7 @@
 hungry = input("are you hungry?")
 if hungry == "yes":
     print("eat pelmeni")
+if hungry == "wtf"
+    print("cho ti?")
 else:
-    print ("do you homework")
+    print("sorry dont worries")
